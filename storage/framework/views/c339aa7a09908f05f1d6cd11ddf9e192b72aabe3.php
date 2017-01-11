90b7bd92ac2e9fee1675611d4a1dca4faa38914d
aa.Tessst <1072147612@qq.com>
2017-01-11 11:58:05 +0800

    Revert "整体第一次上传,后期维护更新"
    This reverts commit ba9269f24e15f2c4c14e06daaa46ce96a4d63e8f.

--- storage/framework/views/c339aa7a09908f05f1d6cd11ddf9e192b72aabe3.php
+++
@@ -1,7 +0,0 @@
-
-<?php echo $__env->make('index.public.head', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-
-  <div style="width: 80%; height: 800px; border-radius: 5px; border: red 1px solid; margin-left: auto; margin-right: auto"></div>
-
-<?php echo $__env->make('index.public.tail', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-
